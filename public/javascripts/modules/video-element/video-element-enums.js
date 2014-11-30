183f42741d376a78a14300cad51f2eb3e4a0c6f7
cujojp <info@cujo.jp>
2014-11-29 20:50:53 -0800

    moving files to modules

---
+++ public/javascripts/modules/video-element/video-element-enums.js
@@ -0,0 +1,13 @@
+(function($, app) {
+  'use strict';
+  
+  app._Modules.VideoElement.Enums = {
+
+    /** @enum {string} */
+    ClassName: {
+      BASE: 'video-player'
+    }
+
+  };
+})(jQuery, Cujo);
+
