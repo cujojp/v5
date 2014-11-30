476e90607bffb3af6195933354d18afbe0532171
cujojp <info@cujo.jp>
2014-11-30 00:59:30 -0800

    added autoprexifxer and working on blur.
    + Seems the only option maybe to use canavs

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var autoprefix = require('gulp-autoprefixer');
 var recess = require('gulp-recess');
 var handleErrors = require('../utils/handleErrors');
 var notify = require('gulp-notify');
@@ -7,6 +8,7 @@ var notify = require('gulp-notify');
 gulp.task('styles', function () {
   gulp.src('./public/sass/**/*.scss')
     .pipe(sass())
+    .pipe(autoprefix())
     .on('error', function (err) { console.log(err.message); })
     .pipe(gulp.dest('./public/stylesheets'));
 });
