8fb6e5a6e5b29bd284f7608b1f57cea9c2382e5a
cujojp <info@cujo.jp>
2014-11-29 20:50:53 -0800

    moving files to modules

---
+++ public/javascripts/modules/video-element/index.js
@@ -0,0 +1,23 @@
+//= include ./video-element.js
+//= include ./video-element-enums.js
+
+/**
+ * @fileoverview
+ * @author Kaleb White (cujojp)
+ *    
+ * Instantiates new videoElement class if need be. 
+ * Based on if elements are found on the documennt.
+ *
+ */
+
+(function( $, app ) {
+  'use strict';
+
+  var $module = $('.'+app._Modules.VideoElement.Enums.ClassName.BASE);
+
+  // Initialize Modules that don't require additional configuration.
+  if ($module.length) {
+    app._Modules.VideoElement.init();
+  }
+
+})(jQuery, Cujo);
---
+++ public/javascripts/modules/video-element/video-element.js
@@ -0,0 +1,69 @@
+/**
+ * @fileoverview
+ * @author Kaleb White (cujojp)
+ *    
+ * Controls the height and width of a video element
+ * and keeps video elements aspect ratio based on
+ * documents height. Also displays a fallback image
+ * if inline video elements are not supported (ie mobile).
+ */
+
+(function( $, app ) {
+
+  var module = {
+    init: function() {
+      var $module = $('.'+app._Modules.VideoElement.Enums.ClassName.BASE);
+
+      $module.each(function() {
+        new VideoElement(this);
+      });
+    }
+  };
+
+
+  /**
+   * Header Constructor
+   * @constructor
+   */
+  var VideoElement = function(options) {
+
+    /**
+     * Defaults Object
+     * TODO (kaleb): should be public header enum.
+     * @type {object}
+     * @private
+     */
+    this._defaults = {};
+
+    /**
+     * Base options object.
+     * @type {object}
+     * @private
+     */
+    this._options = {};
+
+    // Map optional configs if they exist
+    options = this._options = options ?
+          $.extend({}, this._defaults, options) :
+          this._defaults;
+
+
+    this._init();
+  };
+
+  /**
+   * _init
+   * Initializes the header
+   *
+   * @return
+   */
+  VideoElement.prototype._init = function() {
+
+    console.log('hey cool were running');
+
+  };
+
+  app._Modules.VideoElement = module;
+
+})(jQuery, Cujo);
+
