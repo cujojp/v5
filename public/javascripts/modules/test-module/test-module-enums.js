e89e18dffdc680bdca592b1514ae3f740b133298
cujojp <info@cujo.jp>
2014-12-02 23:51:20 -0800

    added quick test module scaffold

---
+++ public/javascripts/modules/test-module/test-module-enums.js
@@ -0,0 +1,13 @@
+(function($, app) {
+  'use strict';
+  
+  app._Modules.TestModule.Enums = {
+
+    /** @enum {string} */
+    ClassName: {
+      BASE: 'test-module'
+    }
+
+  };
+})(jQuery, Cujo);
+
