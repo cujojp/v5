717c48ae9c3295ae0a69b89123fa7b598ecdb6c1
cujojp <info@cujo.jp>
2015-01-02 10:57:45 -0600

    lowering scores because... objects + also removed some logs

--- gulp/tasks/svg.js
+++ gulp/tasks/svg.js
@@ -2,12 +2,10 @@ var gulp = require('gulp');
 var svg = require('gulp-svg-sprites');
 var plumber = require('gulp-plumber');
 var handleErrors = require('../utils/handleErrors');
-var filelog = require('gulp-filelog');
 var notify = require('gulp-notify');
 
 gulp.task('svg', function () {
   gulp.src(['./public/svg/icons/**/*.svg', '!./public/svg/patterns/*.svg'])
-    .pipe( filelog() )
     .pipe( svg({
       mode: "symbols",
       preview: false,
