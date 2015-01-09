38451d526959f2b918248e394f1def999e3cdd6f
cujojp <info@cujo.jp>
2015-01-08 19:41:05 -0800

    build shouldnt run complexity

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,3 +1,3 @@
 var gulp = require('gulp');
 
-gulp.task('build', ['startexpress', 'svg', 'scripts-complexity', 'scripts', 'styles']);
+gulp.task('build', ['startexpress', 'svg', 'scripts', 'styles']);
